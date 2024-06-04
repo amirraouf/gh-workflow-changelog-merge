d0b257a25ec03571cf918b098880da55e4fbdd8f
Amir Raouf <amir2raouf@gmail.com>
2024-06-04 17:39:48 +0300

    fix workflow

--- app/_version.py
+++ app/_version.py
@@ -5,7 +5,10 @@ Provides app version information.
 # This file is auto-generated! Do not edit!
 # Use `python -m incremental.update app` to change this file.
 
-from incremental import Version
+from incremental import Version, getVersionString
 
 __version__ = Version("app", 0, 95, 0)
 __all__ = ["__version__"]
+
+if __name__ == '__main__':
+    print(getVersionString(__version__).split(" ")[1])
